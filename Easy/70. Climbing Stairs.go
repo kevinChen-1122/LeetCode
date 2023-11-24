b69277e9abb0e64d4c04acc2ab5a5155e3cbe865
Kevin <fortis013@gmail.com>
2023-11-24 18:36:30 +0800

    resolved question

---
+++ Easy/70. Climbing Stairs.go
@@ -0,0 +1,11 @@
+package Easy
+
+// https://leetcode.com/problems/climbing-stairs/
+
+func climbStairs(n int) int {
+	next, secondNext := 0, 1
+	for ; n > 0; n-- {
+		next, secondNext = secondNext, next+secondNext
+	}
+	return secondNext
+}
